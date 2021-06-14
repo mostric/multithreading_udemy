00259ca4b555e87f25202465c3ed5b62358d8994
Ruslan Mostov <dabuita@live.com>
2021-06-14 13:24:05 +0300

    Part 2 - hacker\police races

---
+++ src/main/java/multithreading/udemy/example1/Main.java
@@ -0,0 +1,16 @@
+package multithreading.udemy.example1;
+
+public class Main {
+    public static void main(String[] args) {
+        Thread thread1 = new NewThread();
+        thread1.start();
+    }
+
+    private static class NewThread extends Thread {
+        @Override
+        public void run() {
+            // Code that run in a new thread
+            System.out.println("Hello from " + this.getName());
+        }
+    }
+}
